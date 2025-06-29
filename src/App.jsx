b7ca46c41f7abc618a6468da51cee8a68ed09d0e
siyam7 <muhammedsiyam7@gmail.com>
2025-06-29 18:53:57 +0530

    change number array to object(id, value, isHeld)array

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import { useState } from "react"
+import { nanoid } from "nanoid"
 import "./App.css"
 import Die from "./components/Die.jsx"
 export default function App(){
 
-  const [numbers, setNumbers] = useState(generateAllNewDice());
-  const dieArray = numbers.map(val=><Die value={val}/>)
+ const [dice, setDice] = useState(generateAllNewDice());
   function generateAllNewDice(){
     return new Array(10)
-    .fill(0)
-    .map(()=>Math.ceil(Math.random()*6))
+    .fill(null)
+    .map(()=>({
+      value: Math.ceil(Math.random()*6),
+      isHeld: false,
+      id: nanoid()
+    }))
+    
   }
+  function handleRoll(){
+    setDice(generateAllNewDice())
+  }
+  const dieArray = dice.map(dieObj => <Die key={dieObj.id} value={dieObj.value}/>)
+
 
   return(
     <main>
       <div className="die-container">
-      {dieArray}
+       {dieArray}
       </div>
+      <button onClick={handleRoll}>
+        Roll
+      </button>
     </main>
   )
 }
